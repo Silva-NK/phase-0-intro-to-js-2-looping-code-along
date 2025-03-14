b5acd4230acde9e4874d9d6a09c9e29e9fa8a06e
Silva-NK <kiagonsuzanne@gmail.com>
2025-03-15 02:28:22 +0300

    Add functions writeCards and countDown to index.js.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const names = ["Gemma", "Jonah", "Nina", "Rob", "Ivan", "Juliet"];
+const event = "graduation";
+
+function writeCards(names, event) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
+console.log(writeCards(names, event));
+
+
+
+function countDown() {
+    let i = 10;
+
+    while (i >= 0){
+        console.log(i--);
+        ;
+    }
+
+    return i;
+}
+
+countDown();
